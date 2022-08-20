4d6f349d033e03afeb96456b8a26834d9ccc9458
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-08-20 15:51:28 +0200

    CREATION Formulaire Users

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -8,7 +8,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class UsersType extends AbstractType
 {
@@ -17,18 +20,42 @@ class UsersType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'required' => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 255,
+                    ]),
+                ],
                 'label' => 'Adresse Email'
-            ])
-            ->add('password')
-            ->add('roles', ChoiceType::class, [
+                ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques',
+                'label' => false,
                 'required' => true,
-                'choices'  => [
-                    'Selectionnez un rôle...' => null,
-                    'Équipe technique de Green FIT' => 'ROLE_ADMIN',
-                    'Partenaire de Green FIT' => 'ROLE_PARTNER',
-                    'Struture de Green FIT' => 'ROLE_STRUCTURE',                    
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir un mot de passe'
+                    ]
                 ],
             ])
+            // ->add('roles', ChoiceType::class, [
+            //     'required' => true,
+            //     'choices'  => [
+            //         'Selectionnez un rôle...' => null,
+            //         'Équipe technique de Green FIT' => 'ROLE_ADMIN',
+            //         'Partenaire de Green FIT' => 'ROLE_PARTNER',
+            //         'Struture de Green FIT' => 'ROLE_STRUCTURE',                    
+            //     ],
+            // ])
             ->add('lastname', TextType::class, [
                 'required' => true,
                 'label' => 'Nom de famille'
@@ -44,7 +71,6 @@ class UsersType extends AbstractType
             ->add('zipcode', TextType::class, [
                 'required' => true,
                 'label' => 'Code postal',
-                'attr' => ['pattern' => '/^[0-9]{8}$/', 'maxlength' => 5]
             ])
             ->add('city', TextType::class, [
                 'required' => true,
