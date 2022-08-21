d9419976de961e8371a7866972fef2e34dadf02b
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-08-21 23:29:58 +0200

    MERGE CONFLICT dans PersonType

--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -48,10 +48,6 @@ class PersonType extends AbstractType
                 'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques',
                 'label' => false,
                 'required' => true,
-<<<<<<< HEAD
-               
-=======
->>>>>>> origin
                 'first_options' => [
                     'label' => 'Mot de passe',
                     'attr' => [
